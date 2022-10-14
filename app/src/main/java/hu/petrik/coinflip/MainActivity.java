1a04adafdf9fe902df5604bb83f1cbb9906aa9e2
FZsiga02 <fzsigmond02@gmail.com>
2022-10-14 15:57:59 +0200

    Alert

--- app/src/main/java/hu/petrik/coinflip/MainActivity.java
+++ app/src/main/java/hu/petrik/coinflip/MainActivity.java
@@ -1,8 +1,10 @@
 package hu.petrik.coinflip;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,9 +20,12 @@ public class MainActivity extends AppCompatActivity {
      TextView dobasok;
      TextView gyozelem;
      TextView vereseg;
+     AlertDialog.Builder alert;
 
+     //0 - ÍRÁS, 1 - FEJ
      private int tipp;
      private int dobas;
+
      private int dobasCounter = 0;
      private int gyozelemCounter = 0;
      private int veresegCounter = 0;
@@ -36,17 +41,15 @@ public class MainActivity extends AppCompatActivity {
         dobasok = (TextView) findViewById(R.id.dobasCounter);
         gyozelem = (TextView) findViewById(R.id.gyozelemCounter);
         vereseg = (TextView) findViewById(R.id.veresegCounter);
+        alert = new AlertDialog.Builder(this);
 
         fej.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 tipp = 1;
-                dobas = (int)Math.random() * 1;
-                kep.setImageResource(R.drawable.heads);
-                Toast.makeText(getApplicationContext(), "Fej", Toast.LENGTH_SHORT).show();
-                dobasCounter++;
-                dobasok.setText(String.valueOf(dobasCounter));
+                botDobas();
                 ellenorzes();
+                vege();
             }
         });
 
@@ -54,18 +57,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 tipp = 0;
-                dobas = (int)Math.random() * 1;
-                kep.setImageResource(R.drawable.tails);
-                Toast.makeText(getApplicationContext(), "Írás", Toast.LENGTH_SHORT).show();
-                dobasCounter++;
-                dobasok.setText(String.valueOf(dobasCounter));
+                botDobas();
                 ellenorzes();
+                vege();
             }
         });
 
 
 
     }
+
+    private void botDobas(){
+        dobas = (int)(Math.random() * 2);
+        if (dobas == 1){
+            kep.setImageResource(R.drawable.heads);
+            Toast.makeText(getApplicationContext(), "Fej", Toast.LENGTH_SHORT).show();
+            dobasCounter++;
+            dobasok.setText(String.valueOf(dobasCounter));
+        } else {
+            dobas = (int)(Math.random() * 2);
+            kep.setImageResource(R.drawable.tails);
+            Toast.makeText(getApplicationContext(), "Írás", Toast.LENGTH_SHORT).show();
+            dobasCounter++;
+            dobasok.setText(String.valueOf(dobasCounter));
+        }
+    }
+
     private void ellenorzes(){
         if (tipp == dobas){
             gyozelemCounter++;
@@ -73,6 +90,58 @@ public class MainActivity extends AppCompatActivity {
         } else {
             veresegCounter++;
             vereseg.setText(String.valueOf(veresegCounter));
+        }
+    }
+
+    private void vege(){
+        if (dobasCounter == 5){
+            if (gyozelemCounter > veresegCounter){
+                alert.setTitle("Győzelem").
+                        setMessage("Szeretne új játékot játszani?").
+                        setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                                System.exit(0);
+                            }
+                        }).
+                        setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                kep.setImageResource(R.drawable.heads);
+                                dobasCounter = 0;
+                                dobasok.setText(String.valueOf(dobasCounter));
+                                gyozelemCounter = 0;
+                                gyozelem.setText(String.valueOf(gyozelemCounter));
+                                veresegCounter = 0;
+                                vereseg.setText(String.valueOf(veresegCounter));
+                            }
+                        });
+                alert.show();
+            } else {
+                alert.setTitle("Vereség").
+                        setMessage("Szeretne új játékot játszani?").
+                        setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                                System.exit(0);
+                            }
+                        }).
+                        setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                kep.setImageResource(R.drawable.heads);
+                                dobasCounter = 0;
+                                dobasok.setText(String.valueOf(dobasCounter));
+                                gyozelemCounter = 0;
+                                gyozelem.setText(String.valueOf(gyozelemCounter));
+                                veresegCounter = 0;
+                                vereseg.setText(String.valueOf(veresegCounter));
+                            }
+                        });
+                alert.show();
+            }
         }
     }
 }
